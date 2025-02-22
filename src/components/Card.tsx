4d204c1b8a384bafe001e7fda020d8fc6e0e1a9c
hajar-nasr <hajar.nasr.dev@gmail.com>
2025-02-22 21:46:04 +0200

    feat: add suffle animation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,22 @@
+import { motion } from "framer-motion";
+
 const Card = ({ image, onClick }: { image: string; onClick: () => void }) => {
   return (
-    <div
-      className="flex items-center justify-center min-w-24 min-h-24 md:min-w-32 md:min-h-32 bg-white rounded-2xl shadow-lg cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl"
-      onClick={onClick}
+    <motion.div
+      key={image}
+      layout
+      initial={{ opacity: 0, scale: 0.8 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.8 }}
+      transition={{ duration: 0.4, type: "spring" }}
     >
-      <img src={image} alt="pokemon" className="w-3/4 h-3/4 object-contain" />
-    </div>
+      <div
+        className="flex items-center justify-center min-w-24 min-h-24 md:min-w-32 md:min-h-32 bg-white rounded-2xl shadow-lg cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl"
+        onClick={onClick}
+      >
+        <img src={image} alt="pokemon" className="w-3/4 h-3/4 object-contain" />
+      </div>
+    </motion.div>
   );
 };
 
