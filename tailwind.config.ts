3127868a5ebda98af0b6016244368c23183ebc3b
hajar-nasr <hajar.nasr.dev@gmail.com>
2025-02-22 16:51:26 +0200

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Paths to your source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
